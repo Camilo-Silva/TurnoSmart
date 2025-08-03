412f136628ad3cc4c339a7d0fdf204195d152b5f
Camilo-Silva <128434548+Camilo-Silva@users.noreply.github.com>
2025-08-03 02:45:04 -0300

    Fix: Remove incorrect SQL Server migrations that caused nvarchar errors

--- obj/Debug/net8.0/turno-smart.AssemblyInfo.cs
+++ obj/Debug/net8.0/turno-smart.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("turno-smart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18eca66dfdc0032f862aaf176d1398acbd4c9ee1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ca3c36bae4f3a38ae63e11383d1f5a140ccc1db")]
 [assembly: System.Reflection.AssemblyProductAttribute("turno-smart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("turno-smart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
